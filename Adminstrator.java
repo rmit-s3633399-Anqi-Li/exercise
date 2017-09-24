03872d52196bdd0b7a74cf36e97d0158c7d94998
rmit-s3633399-Anqi-Li <31309552+rmit-s3633399-Anqi-Li@users.noreply.github.com>
2017-09-24 18:01:08 +1000

    Add files via upload

---
+++ Adminstrator.java
@@ -0,0 +1,54 @@
+package Administrator;
+
+public class Adminstrator<login> {
+	public String name;
+	public int phone;
+	public String address;
+	public String email;
+	public String userID;
+    public String password;
+    private login login;
+	public login getLogin() {
+		return login;
+	}
+	public void setLogin(login login) {
+		this.login = login;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
